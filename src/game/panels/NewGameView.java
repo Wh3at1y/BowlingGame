078773973fdfcd9020e87fd65573868b9ab11e9e
PrestonLabsAndTech <anthony@prestonlabsandtechnology.com>
2017-04-23 22:33:16 -0600

    Reformatted NewGameView

--- src/game/panels/NewGameView.java
+++ src/game/panels/NewGameView.java
@@ -17,22 +17,12 @@ public class NewGameView extends JPanel
 		public NewGameView(AllViewsPanel allViewsPanel)
 			{
 				baseLayout = new SpringLayout();
+				
 				twoplayersLabel = new JLabel("2 Players ");
-				baseLayout.putConstraint(SpringLayout.NORTH, twoplayersLabel, 620, SpringLayout.NORTH, this);
-				baseLayout.putConstraint(SpringLayout.WEST, twoplayersLabel, 885, SpringLayout.WEST, this);
 				threeplayersLabel = new JLabel("3 Players ");
-				baseLayout.putConstraint(SpringLayout.NORTH, threeplayersLabel, 720, SpringLayout.NORTH, this);
-				baseLayout.putConstraint(SpringLayout.WEST, threeplayersLabel, 0, SpringLayout.WEST, twoplayersLabel);
 				fourplayersLabel = new JLabel("4 Players ");
-				baseLayout.putConstraint(SpringLayout.NORTH, fourplayersLabel, 820, SpringLayout.NORTH, this);
-				baseLayout.putConstraint(SpringLayout.WEST, fourplayersLabel, 0, SpringLayout.WEST, threeplayersLabel);
-				startGameLabel = new JLabel("Start Game");
-				baseLayout.putConstraint(SpringLayout.NORTH, startGameLabel, 60, SpringLayout.SOUTH, fourplayersLabel);
-				baseLayout.putConstraint(SpringLayout.WEST, startGameLabel, 150, SpringLayout.WEST, fourplayersLabel);
 				backLabel = new JLabel("Back");
-				baseLayout.putConstraint(SpringLayout.NORTH, backLabel, 0, SpringLayout.NORTH, startGameLabel);
-				baseLayout.putConstraint(SpringLayout.EAST, backLabel, -80, SpringLayout.WEST, fourplayersLabel);
-
+				
 				buildPanel();
 				buildWindow();
 			}
@@ -51,6 +41,16 @@ public class NewGameView extends JPanel
 
 		private void buildWindow()
 			{
+				baseLayout.putConstraint(SpringLayout.NORTH, twoplayersLabel, 620, SpringLayout.NORTH, this);
+				baseLayout.putConstraint(SpringLayout.WEST, twoplayersLabel, 885, SpringLayout.WEST, this);
+				baseLayout.putConstraint(SpringLayout.NORTH, threeplayersLabel, 720, SpringLayout.NORTH, this);
+				baseLayout.putConstraint(SpringLayout.WEST, threeplayersLabel, 0, SpringLayout.WEST, twoplayersLabel);
+				baseLayout.putConstraint(SpringLayout.NORTH, fourplayersLabel, 820, SpringLayout.NORTH, this);
+				baseLayout.putConstraint(SpringLayout.WEST, fourplayersLabel, 0, SpringLayout.WEST, threeplayersLabel);
+				baseLayout.putConstraint(SpringLayout.NORTH, backLabel, 0, SpringLayout.NORTH, startGameLabel);
+				baseLayout.putConstraint(SpringLayout.EAST, backLabel, -80, SpringLayout.WEST, fourplayersLabel);
+
+				
 			}
 
 		public JLabel getStartGameLabel()
