b921a4014417bf29e541d1b3732690d4d4434b4e
StayinNasty <keganswensen@gmail.com>
2017-04-24 01:00:53 -0600

    Username shit

--- src/game/panels/BowlingGameView.java
+++ src/game/panels/BowlingGameView.java
@@ -1,5 +1,6 @@
 package game.panels;
 
+import java.awt.Font;
 import java.awt.Image;
 import game.panels.*;
 import game.view.AllViewsPanel;
@@ -11,13 +12,11 @@ public class BowlingGameView extends JPanel
 		private SpringLayout layout;
 		private JLabel logoImage;
 		private JLabel backgroundImage;
-		private JLabel twoPlayerBoard;
-		private JLabel threePlayerBoard;
-		private JLabel fourPlayerBoard;
 		private JLabel player1Username;
 		private JLabel player2Username;
 		private JLabel player3Username;
 		private JLabel player4Username;
+		private JLabel rollingUsername;
 
 		public BowlingGameView(AllViewsPanel allViewsPanel)
 			{
@@ -28,17 +27,24 @@ public class BowlingGameView extends JPanel
 				logoImage = new JLabel(new ImageIcon(BowlingGameView.class.getResource("/resources/MiniBowlLogo.png")));
 				backgroundImage = new JLabel();
 				
-				player1Username = new JLabel("Player1");	
-				layout.putConstraint(SpringLayout.NORTH, player1Username, 298, SpringLayout.NORTH, this);
-				layout.putConstraint(SpringLayout.WEST, player1Username, 165, SpringLayout.WEST, this);
+				player1Username = new JLabel("Player1");
+				layout.putConstraint(SpringLayout.WEST, player1Username, 57, SpringLayout.WEST, this);
+				layout.putConstraint(SpringLayout.SOUTH, player1Username, -200, SpringLayout.SOUTH, this);
+				player1Username.setFont(new Font("Sitka Text", Font.BOLD, 45));
 				player2Username = new JLabel("Player2");
-				layout.putConstraint(SpringLayout.NORTH, player2Username, 310, SpringLayout.NORTH, this);
-				layout.putConstraint(SpringLayout.EAST, player2Username, -324, SpringLayout.EAST, this);
+				layout.putConstraint(SpringLayout.WEST, player2Username, 0, SpringLayout.WEST, player1Username);
+				layout.putConstraint(SpringLayout.SOUTH, player2Username, 150, SpringLayout.SOUTH, player1Username);
+				player2Username.setFont(new Font("Sitka Text", Font.BOLD, 45));
 				player3Username = new JLabel("Player3");
-				layout.putConstraint(SpringLayout.WEST, player3Username, 77, SpringLayout.WEST, this);
+				layout.putConstraint(SpringLayout.WEST, player3Username, 0, SpringLayout.WEST, player1Username);
+				layout.putConstraint(SpringLayout.SOUTH, player3Username, 300, SpringLayout.SOUTH, player1Username);
 				player4Username = new JLabel("Player4");
-				layout.putConstraint(SpringLayout.WEST, player4Username, 321, SpringLayout.WEST, this);
-				layout.putConstraint(SpringLayout.SOUTH, player4Username, -256, SpringLayout.SOUTH, this);
+				layout.putConstraint(SpringLayout.WEST, player4Username, 0, SpringLayout.WEST, player1Username);
+				layout.putConstraint(SpringLayout.SOUTH, player4Username, 450, SpringLayout.SOUTH, player1Username);
+				rollingUsername = new JLabel("Username");
+				layout.putConstraint(SpringLayout.NORTH, rollingUsername, 75, SpringLayout.NORTH, this);
+				layout.putConstraint(SpringLayout.WEST, rollingUsername, 57, SpringLayout.WEST, this);
+				rollingUsername.setFont(new Font("Sitka Text", Font.BOLD, 45));
 					
 				buildPanel();
 				buildWindow();
@@ -50,6 +56,7 @@ public class BowlingGameView extends JPanel
 			player2Username.setText(this.allViewsPanel.getUsernameView().getUsername2());
 			player3Username.setText(this.allViewsPanel.getUsernameView().getUsername3());
 			player4Username.setText(this.allViewsPanel.getUsernameView().getUsername4());
+			rollingUsername.setText(this.allViewsPanel.getUsernameView().getUsername1());
 		}
 		
 		private void buildPanel()
@@ -62,6 +69,7 @@ public class BowlingGameView extends JPanel
 				this.add(this.player2Username);
 				this.add(this.player3Username);
 				this.add(this.player4Username);
+				this.add(this.rollingUsername);
 			}
 
 		private void buildWindow()
@@ -72,7 +80,6 @@ public class BowlingGameView extends JPanel
 				layout.putConstraint(SpringLayout.WEST, backgroundImage, 0, SpringLayout.WEST, this);
 				layout.putConstraint(SpringLayout.SOUTH, backgroundImage, 0, SpringLayout.SOUTH, this);
 				layout.putConstraint(SpringLayout.EAST, backgroundImage, 0, SpringLayout.EAST, this);
-				layout.putConstraint(SpringLayout.SOUTH, player3Username, -45, SpringLayout.SOUTH, this);
 			}
 
 		public void setPlayer1Username(JLabel player1Username)
