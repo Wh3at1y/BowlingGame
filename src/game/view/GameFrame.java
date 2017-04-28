883de4aaf37718860f5effbc37ef73770db0db2d
Sam Montoya <sam@prestonlabsandtechnology.com>
2017-04-27 18:42:19 -0600

    Added a minibowl splash on startup

--- src/game/view/GameFrame.java
+++ src/game/view/GameFrame.java
@@ -3,7 +3,13 @@
  */
 package game.view;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
 public class GameFrame extends JFrame
@@ -19,8 +25,26 @@ public class GameFrame extends JFrame
 	    catch(Exception e){ 
 	    }
 		
-		basePanel = new AllViewsPanel();
-		buildFrame();
+		JWindow window = new JWindow();
+		window.getContentPane().add(new JLabel("", new ImageIcon(GameFrame.class.getResource("/resources/MiniBowlLogo.png")), SwingConstants.CENTER));
+		//window.setLocationRelativeTo(null);
+		window.setSize(1920, 1080);
+		window.setBackground(new Color(255,255,255,0));
+		window.setVisible(true);
+		try
+		{
+			Thread.sleep(3500);
+			window.dispose();
+			basePanel = new AllViewsPanel();
+			buildFrame();
+		} catch (InterruptedException error)
+		{
+			// TODO Auto-generated catch block
+			error.printStackTrace();
+			
+		}
+		
+		
 	}
 
 	private void buildFrame()
